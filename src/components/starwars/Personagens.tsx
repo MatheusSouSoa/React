882cbca3c33d08fa82cd107ef424e27c0973e446
Matheus Soares <matheus.sousoa.dev@gmail.com>
2023-07-21 14:35:54 -0300

    hooks personalizados

--- src/components/starwars/Personagens.tsx
+++ src/components/starwars/Personagens.tsx
@@ -1,8 +1,53 @@
+import useProcessando from "@/data/hooks/useProcessando";
+import { useState } from "react";
 
 export default function Personagens() {
+
+    const {processando, iniciarProcessamento, finalizarProcessamento} = useProcessando()
+    const [personagens, setPersonagens] = useState<any>([])
+
+    async function simularChamadaAPI() {
+        const resp = await fetch('https://swapi.dev/api/people/')
+        const data = await resp.json()
+        setPersonagens(data.results)
+        //console.log(data.results)
+        // return new Promise((resolve) => {
+        // setTimeout(() => {
+        //     resolve(1);
+        // }, 6000);
+        // });
+    }
+
+    async function obterPersonagens() {
+        try {
+            iniciarProcessamento()
+            await simularChamadaAPI();
+        } finally {
+            finalizarProcessamento()
+        }
+    }
+
+    function renderizarPersonagens() {
+        return (
+            <ul>
+                {personagens.map((p: any) => (
+                    <li key={p.name}>{p.name}</li>
+                ))}
+            </ul>
+        )
+    }
+
     return (
         <div>
-            <h1>Personagens</h1>
+            {processando ? (
+                <div>Processando...</div>
+            ) : personagens.length > 0 ? (
+                renderizarPersonagens()
+            ) : (
+                <div>Nenhum personagem encontrado</div>
+            )}
+
+            <button onClick={obterPersonagens} className="bg-blue-500 p-2">Obter</button>
         </div>
     )
 }
